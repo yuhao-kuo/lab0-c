ccc07e14d160564a987938ac9b766d1ab12164f6
yuhao-kuo <st40201@gmail.com>
2019-09-28 17:51:40 +0800

    Impletement q_new

--- queue.c
+++ queue.c
@@ -26,7 +26,11 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
-    q->head = NULL;
+    if (q != (queue_t *) NULL) {
+        q->head = NULL;
+        q->tail = NULL;
+        q->length = 0UL;
+    }
     return q;
 }
 
